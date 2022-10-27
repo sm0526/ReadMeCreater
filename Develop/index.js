2c427d12ba7cd609fce96b8b11a979dfd7a95d47
Shanon Holland <105896063+sm0526@users.noreply.github.com>
2022-10-26 18:23:57 -0600

    Wrote the function to actually write the file

--- Develop/index.js
+++ Develop/index.js
@@ -62,10 +62,14 @@ const questions = [
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+ }
 
 // TODO: Create a function to initialize app
-function init() { }
+function init() {
+    
+}
 
 // Function call to initialize app
 init();
