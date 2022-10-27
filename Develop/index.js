700e8ca69cd0fc7ff7ed6feae2a2b6dd4c5e3032
Shanon Holland <105896063+sm0526@users.noreply.github.com>
2022-10-26 18:59:16 -0600

    Wrote the function to run the app

--- Develop/index.js
+++ Develop/index.js
@@ -68,7 +68,12 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-    
+    inquirer.prompt(questions).then((inquirerResponses) => {
+        console.log('Creating README...');
+        writeToFile('README.md', generateMarkdown({
+            ...inquirerResponses
+        }));
+    });
 }
 
 // Function call to initialize app
